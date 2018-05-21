33cded130388a8c9f06e7a490e0acc9b51dbd31b
Garrett Hughes <ghughes139+git@gmail.com>
2018-05-20 21:53:02 -0500

    Implimented Search Function

--- js/searchfunct.js
+++ js/searchfunct.js
@@ -1,4 +1,26 @@
-$("#search").keyup(function() {
-  var input = $(this).val();
-  $('.data-title:not(:contains('value'))').hide();
+////////////////////////
+////Search Function/////
+////////////////////////
+
+$('#search').keyup(function() {
+    var value = $(this).val();
+    var exp = new RegExp(value, 'i');
+
+    $('a').each(function() {
+        var match = exp.test($(this).data('title'));
+        $(this).toggle(match);
+    });
 });
+
+
+////////////////////////
+///LightBox Options/////
+////////////////////////
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'showImageNumberLabel': false,
+    'maxWidth': 1000,
+    'maxHeight': 1000
+  })
